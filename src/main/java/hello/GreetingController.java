41d369bb49265d3ca61caa688575ca9437fa48f5
Seliverstow Alexandr <a.v.seliverstow@gmail.com>
2018-12-22 18:50:44 +0400

    learn

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -1,10 +1,20 @@
 package hello;
 
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
@@ -19,7 +29,7 @@ public class GreetingController {
     }
 
     @RequestMapping("/longs")
-    public long trpk(@RequestParam(value = "ints")long longs){
+    public long getLongs(@RequestParam(value = "longs")long longs){
         System.out.println(longs);
         return longs;
     }
@@ -28,4 +38,15 @@ public class GreetingController {
     public String hello(){
         return "hello";
     }
+
+
+    @RequestMapping("/employees")
+    public String handleEmployeeRequestByDept(@RequestParam("dept") String deptName,
+                                              @RequestParam("state") String stateCode,
+                                              Model map) {
+        map.addAttribute("msg", "employees request by dept and state code : " +
+                deptName + ", " + stateCode);
+        return map.toString();
+    }
+
 }
