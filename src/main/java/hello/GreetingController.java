c186e0961e288e3eed88c607c1675c0df4688616
Alexandr Seliverstow <a.v.seliverstow@gmail.com>
2018-12-22 23:07:40 +0400

    add Post method

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -2,12 +2,16 @@ package hello;
 
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
 public class GreetingController {
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
+    //командная строка для cURL
+    //curl -i -X POST http://localhost:8080/
 
     @RequestMapping("/greeting")
     public Greeting greeting(@RequestParam(value="name", required=false, defaultValue="World") String name) {
@@ -33,10 +37,14 @@ public class GreetingController {
         return "empId = " + employeeId + " " + "epmMonths " + previousMonths;
     }
 
-    @RequestMapping(value = "/ex/foos", method = RequestMethod.POST)
+    @RequestMapping(value = "/ex/post", method = RequestMethod.POST)
     @ResponseBody
-    public String postFoos() {
-        return "Post some Foos";
+    public List<String> postFoos() {
+        List<String> list = new ArrayList<>();
+        list.add("Post post post");
+        list.add("aaa aaa aaa");
+        list.add("bbb bbb bbb");
+        return list;
     }
 
 }
